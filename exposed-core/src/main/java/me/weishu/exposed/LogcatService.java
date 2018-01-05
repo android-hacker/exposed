1ccaec06cb3a3b2763528cc189d0ef07634757cb
维术 <weishu.tws@alipay.com>
2018-01-05 22:32:17 +0800

    [Exposed-Fix] Xposed log support.

---
+++ exposed-core/src/main/java/me/weishu/exposed/LogcatService.java
@@ -0,0 +1,95 @@
+package me.weishu.exposed;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.robv.android.xposed.XposedHelpers;
+
+/**
+ * Created by weishu on 18/1/5.
+ */
+public class LogcatService extends Service {
+
+    private static final String TAG = "LogcatService";
+
+    private volatile boolean mReading = false;
+
+    private static final String PATH_KEY = "path";
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String path = intent.getStringExtra(PATH_KEY);
+        if (!TextUtils.isEmpty(path)) {
+            startReadLogcat(path);
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void startReadLogcat(final String path) {
+        if (mReading) {
+            return;
+        }
+        new Thread("exposed-logcat") {
+            @Override
+            public void run() {
+                super.run();
+                mReading = true;
+                try {
+                    Log.i(TAG, "exposed logcat start..");
+                    List<String> cmds = new ArrayList<String>();
+                    cmds.add("sh");
+                    cmds.add("-c");
+                    cmds.add("logcat -v time -s XposedStartupMarker:D Xposed:I appproc:I XposedInstaller:I art:F DexposedBridge:I >> " + path);
+                    ProcessBuilder pb = new ProcessBuilder(cmds);
+                    Process p = pb.start();
+                    p.waitFor();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    public static void start(Context context, File xposedInstallerDataDir) {
+        Intent t = new Intent(context, LogcatService.class);
+        File logDir = new File(xposedInstallerDataDir, "exposed_log");
+        if (!(logDir.exists() && logDir.isDirectory())) {
+            logDir.mkdir();
+            setWorldReadable(logDir);
+        }
+
+        File logFile = new File(logDir, "error.log");
+        if (!(logFile.exists() && logFile.isFile())) {
+            try {
+                logFile.createNewFile();
+                setWorldReadable(logFile);
+            } catch (IOException ignored) {
+            }
+        }
+
+        t.putExtra(PATH_KEY, logFile.getPath());
+        context.startService(t);
+    }
+
+    private static void setWorldReadable(File file) {
+        XposedHelpers.callStaticMethod(XposedHelpers.findClass("android.os.FileUtils", ClassLoader.getSystemClassLoader()),
+                "setPermissions", file.getPath(),
+                00400 | 00200 | 00100 | 00040 | 00010 | 00004 | 00001, -1, -1);
+    }
+}
